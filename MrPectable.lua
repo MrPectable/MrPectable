edc66a4d0cf55bc0c21ab9681c9f7e33b0a03297
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-02-27 19:52:08 -0500

    Update MrPectable.lua

--- MrPectable.lua
+++ MrPectable.lua
@@ -18,9 +18,9 @@ end
 end)
 
 local Settings = {
-	['Material'] = Enum.Material.Neon, -- Material
+	['Material'] = Enum.Material.Glass, -- Material
 	['Color'] = Color3.fromRGB(0,255,255), -- Color
-	['Transparency'] = 0.4 -- 0 to 1 Transparency
+	['Transparency'] = 0.1 -- 0 to 1 Transparency
 }
 
 local ScreenGui = Instance.new('ScreenGui', game.CoreGui) -- Create screengui
@@ -77,38 +77,4 @@ while game:GetService('RunService').RenderStepped:Wait() do -- loop this process
 	end
 end
 print('MrPectable active..')
---- Tut
- 
-local esp_settings = { ---- table for esp settings 
-    textsize = 8,
-    colour = 255,255,255
-}
- 
-local gui = Instance.new("BillboardGui")
-local esp = Instance.new("TextLabel",gui) ---- new instances to make the billboard gui and the textlabel
- 
- 
- 
-gui.Name = "Cracked esp"; ---- properties of the esp
-gui.ResetOnSpawn = false
-gui.AlwaysOnTop = true;
-gui.LightInfluence = 0;
-gui.Size = UDim2.new(1.75, 0, 1.75, 0);
-esp.BackgroundColor3 = Color3.fromRGB(255, 255, 255);
-esp.Text = ""
-esp.Size = UDim2.new(0.0001, 0.00001, 0.0001, 0.00001);
-esp.BorderSizePixel = 4;
-esp.BorderColor3 = Color3.new(esp_settings.colour)
-esp.BorderSizePixel = 0
-esp.Font = "GothamSemibold"
-esp.TextSize = esp_settings.textsize
-esp.TextColor3 = Color3.fromRGB(esp_settings.colour) -- text colour
- 
-game:GetService("RunService").RenderStepped:Connect(function() ---- loops faster than a while loop :)
-    for i,v in pairs (game:GetService("Players"):GetPlayers()) do
-        if v ~= game:GetService("Players").LocalPlayer and v.Character.Head:FindFirstChild("Cracked esp")==nil  then -- craeting checks for team check, local player etc
-            esp.Text = "{"..v.Name.."}"
-            gui:Clone().Parent = v.Character.Head
-    end
-end
-end)
+
