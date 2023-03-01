a64d8493d4f0a3172ad569687ed7d568c0fdcf2a
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-02-28 23:39:30 -0500

    Update MrPectable.lua

--- MrPectable.lua
+++ MrPectable.lua
@@ -5,7 +5,7 @@ local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/R
 --Lib:CreateWindow(name,DefTab,WinSize,function)
 local Win = Lib:CreateWindow("MrPectable",1,nil,nil)
 
-for i = 1, 16 do
+
 
 --Create Tab
 local Tab,name = Win:CreateTab("Main"..tostring(i),function() warn(i) end)
@@ -18,7 +18,6 @@ Tab:CreateButton("Console",function()
 	game:GetService("StarterGui"):SetCore("DevConsoleVisible",true)
 	end)
 
-for i = 1, i do
 
 
 
