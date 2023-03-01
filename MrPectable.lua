17648132279add7f15ea37eba1dc8a9b424e35e9
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-02-28 23:28:20 -0500

    Update MrPectable.lua

--- MrPectable.lua
+++ MrPectable.lua
@@ -1,17 +1,5 @@
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Refinement/main/InfinitiveUI",true))()
        
-	KeySystem = true, 
-	KeySettings = {
-		Title = "MrHub",
-		Subtitle = "Key System",
-		Note = "Join the discord (https://discord.gg/f6QWTHeEkd)",
-		FileName = "MrKey",
-		SaveKey = true,
-		GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-		Key = "MrPectable"
-	}
-		
-})
 
 
 
