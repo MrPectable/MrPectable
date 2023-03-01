6b60131b9ea42ecb41c07f0509b0793166157eeb
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-02-28 21:51:21 -0500

    Update MrPectable.lua

--- MrPectable.lua
+++ MrPectable.lua
@@ -1,3 +1,9 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/juywvm/ui-libs/main/Valiant_UI_Lib/source.lua"))()
 
 local Window = Library:CreateWindow(<string>, MrPectable, <string>, BETA, <string>, 6071020689)
+
+local Tab = Window:CreateTab(<string> Main Area)
+
+local Page = Tab:CreateFrame(<string> Player)
+
+local Page = Tab:CreateFrame(<string> Scripts)
