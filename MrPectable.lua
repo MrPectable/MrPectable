167bedfade4038fbedbdc9becd0e17bf3cc24515
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-02-28 23:52:50 -0500

    Update MrPectable.lua

--- MrPectable.lua
+++ MrPectable.lua
@@ -5,7 +5,7 @@ local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/R
 --Lib:CreateWindow(name,DefTab,WinSize,function)
 local Win = Lib:CreateWindow("MrPectable",1,nil,nil)
 
-
+for i = 1, 16 do
 
 --Create Tab
 local Tab,name = Win:CreateTab("Main"..tostring(i),function() warn(i) end)
@@ -13,13 +13,6 @@ local Tab,name = Win:CreateTab("Main"..tostring(i),function() warn(i) end)
 Tab:CreateButton("Infinite yield",function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source",true))()
 	end)
-
-Tab:CreateButton("Console",function()
-	game:GetService("StarterGui"):SetCore("DevConsoleVisible",true)
-	end)
-
-
-
-
-end
-end
+for i = 1, i do
+	
+	end
