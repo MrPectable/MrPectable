852ea97e528cd09877b6cd0c74390320ec4052f5
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-03-01 15:32:39 -0500

    Update MrPectable.lua

--- MrPectable.lua
+++ MrPectable.lua
@@ -555,6 +555,8 @@ local btns = serv:Channel("More Soon")
 local lbls = serv:Channel("Credits")
 
 lbls:Label("Credits & INFO")
+lbls:Label("Helper-‘a#7585)
+lbls:Label("Helper-Deity#1001")
 
 serv:Channel("MrPectable#2662")
 
