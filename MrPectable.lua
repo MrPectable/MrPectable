2ec90d21261ec9fe34672799f94198d17ef30d08
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-02-28 23:29:46 -0500

    Update MrPectable.lua

--- MrPectable.lua
+++ MrPectable.lua
@@ -1,6 +1,8 @@
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Refinement/main/InfinitiveUI",true))()
        
-
+--Create Window
+--Lib:CreateWindow(name,DefTab,WinSize,function)
+local Win = Lib:CreateWindow("MrHub",1,nil,nil)
 
 
 
