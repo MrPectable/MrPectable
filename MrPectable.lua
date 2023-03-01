53f83934af1b549c85b8397e96813125a7beee25
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-02-28 23:36:02 -0500

    Update MrPectable.lua

--- MrPectable.lua
+++ MrPectable.lua
@@ -3,12 +3,12 @@ local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/R
 
 --Create Window
 --Lib:CreateWindow(name,DefTab,WinSize,function)
-local Win = Lib:CreateWindow("ah",1,nil,nil)
+local Win = Lib:CreateWindow("MrPectable",1,nil,nil)
 
 for i = 1, 16 do
 
 --Create Tab
-local Tab,name = Win:CreateTab("Tab "..tostring(i),function() warn(i) end)
+local Tab,name = Win:CreateTab("Main"..tostring(i),function() warn(i) end)
 
 Tab:CreateButton("Infinite yield",function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source",true))()
@@ -20,49 +20,6 @@ Tab:CreateButton("Console",function()
 
 for i = 1, i do
 
-
---Returns UI
---Tab:CreateButton(name,function)
-	
-Tab:CreateButton("Button "..i,function()
-	print(i,name)
-	end)
-
-
---Returns bool,UI
---Tab:CreateToggle(name,default,function)
-	
-Tab:CreateToggle("Toggle "..i,false,function(t)
-	print(i,name,t)
-	end)
-
-
---Returns value,UI
---Tab:CreateSlider(name,min,max,default,function)
-
-local max = math.random(500,5000)
-Tab:CreateSlider("Slider "..i,50,max,max/math.random(2,6),function(v)
-	print(i,name,v)
-	end)
-
-
---Returns two userdata,UI
---Tab:CreateDropdown(name,{table,string},visible,function)
-
-Tab:CreateDropdown("Dropdown "..i, {{
-	"Named", {}},"hello","he","ah","eh","yw"
-},false,function(c,f)
-	print(i,name,c,f)
-	end)
-
-
---Returns TextBox for FocusLost or Stretchability
-local Textbox = Tab:CreateTextbox("TextBox "..i,"FFlag")
-
-Textbox:GetPropertyChangedSignal("Text"):Connect(function()
-	local self = Textbox
-	print(self.Text)
-	end)
 
 
 end
