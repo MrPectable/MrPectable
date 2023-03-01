22ea42984ce9439b540623f250da85ef02d8e1ed
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-02-28 23:25:59 -0500

    Update MrPectable.lua

--- MrPectable.lua
+++ MrPectable.lua
@@ -1,19 +1,5 @@
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
-
-local Window = Rayfield:CreateWindow({
-	Name = "MrHub Window",
-	LoadingTitle = "MrHub Universial",
-	LoadingSubtitle = "by MrPectable",
-	ConfigurationSaving = {
-		Enabled = true,
-		FolderName = nil, -- Create a custom folder for your hub/game
-		FileName = "Mr Hub"
-	},
-        Discord = {
-        	Enabled = false,
-        	Invite = "https://discord.gg/f6QWTHeEkd", -- The Discord invite code, do not include discord.gg/
-        	RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-        },
+       
 	KeySystem = true, -- Set this to true to use our key system
 	KeySettings = {
 		Title = "MrHub",
