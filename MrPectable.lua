836862ea99a194198f150244e9e400a405ff198d
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-03-03 18:52:33 -0500

    Update MrPectable.lua

--- MrPectable.lua
+++ MrPectable.lua
@@ -696,7 +696,32 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/OneProtocol/Project/m
 DiscordLib:Notification("Notification", "Executed !", "Okay!")
 end
 )
-
+btns:Button(
+"Auto-Collect Nearby",
+function()
+autocollectitems = true 
+while autocollectitems do wait()
+    pcall(function()
+for i,v in pairs(workspace.Islands:GetDescendants()) do 
+  if v.Name == "Drops" then 
+    for i2,v2 in pairs(v:GetChildren()) do 
+            if v2:IsA("Tool") then 
+                local args = {
+                 [1] = {
+                 ["tool"] = v2,
+               ["player_tracking_category"] = "join_from_web"
+             }
+            }
+              game:GetService("ReplicatedStorage").rbxts_include.node_modules.net.out._NetManaged.CLIENT_TOOL_PICKUP_REQUEST:InvokeServer(unpack(args))
+             end 
+        end 
+    end 
+end 
+end) 
+end 
+DiscordLib:Notification("Notification", "Executed !", "Okay!")
+end
+)
 
 local btns = serv:Channel("More Soon")
 local lbls = serv:Channel("Credits")
